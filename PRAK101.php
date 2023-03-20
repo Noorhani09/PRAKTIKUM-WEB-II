4304a036930e4c8df1c5423be5bc0744251f1ff8
Noorhani09 <114984404+Noorhani09@users.noreply.github.com>
2023-03-20 20:40:16 +0800

    Add files via upload

---
+++ PRAK101.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "Hello World in PHP<br>";
+echo "Nama Praktikan : Noorhani<br>";
+echo "NIM : 2110817120002";
+
+?>
---
+++ PRAK103.php
@@ -0,0 +1,12 @@
+<?php
+$celcius = 37.841;
+
+$fahrenheit = (9/5) * $celcius + 32;
+$reamur = (4/5) * $celcius;
+$kelvin = $celcius + 273.15;
+
+echo "Fahrenheit (F) = "; echo "$fahrenheit<br>";
+echo "Reamur (R) = "; echo "$reamur<br>";
+echo "Kelvin (K) = "; echo "$kelvin";
+
+?>
